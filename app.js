e63b47a38cde953bc850230cf37e6f1e20263f95
Faizan533 <faizankhan.dae@gmail.com>
2020-12-09 13:53:44 +0500

    initial

---
+++ app.js
@@ -0,0 +1,42 @@
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+  var a =document.getElementById('a')
+  var b =document.getElementById('b')
+  
+  switch (n) {
+    case 1:
+      b.classList.remove('orange-b')
+      a.classList.add('orange-b')
+      break;
+    case 2:
+      a.classList.remove('orange-b')
+      b.classList.add('orange-b')
+      break;
+    
+  }
+
+}
+
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  if (n > x.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" w3-red", "");
+  }
+  x[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " w3-red";
+}
